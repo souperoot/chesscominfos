577319d2df5f4c6845f909a932e707d65733ead3
souperoot <118569123+souperoot@users.noreply.github.com>
2023-03-19 13:24:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import json
 import requests
 
-print('''
-________         .__                          
-\_____  \   ____ |  |__   ____   ______ ______
- /   |   \_/ ___\|  |  \_/ __ \ /  ___//  ___/
-/    |    \  \___|   Y  \  ___/ \___ \ \___ \ 
-\_______  /\___  >___|  /\___  >____  >____  >
-        \/     \/     \/     \/     \/     \/ 
-''')
-
 infos_gets = []
 info = ['avatar', 'name', 'username', 'url', 'twitch_url', 'status', 'title', 'followers', 'player_id', 'location']
 def infos(user):
